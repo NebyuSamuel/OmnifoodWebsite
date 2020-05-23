97a0163463c33f4e4ce62aafdb2894b801b5d146
Nebyu Samuel <nebahanijesus12345@gmail.com>
2020-05-24 00:04:04 +0300

    nav icon, performance optimization and SEO added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  /* For the sticky nav */
   $(".js--section-features").waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -8,4 +9,18 @@ $(document).ready(function(){
   }, {
     offset : "60px;"
   });
+
+  $('.js--mobile-nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--mobile-nav-icon i');
+    nav.slideToggle(200);
+    if(icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
 });
